9fd83a91448cea95e339280b579169974d7bfc9a
Максим Сорокiн <sormax04@gmail.com>
2023-10-29 00:07:42 +0300

    first commit

---
+++ vite-tailwind/src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import axios from "axios";
+
+const students = ref([]);
+
+onMounted(async () => { 
+  try {
+    const response = await axios.get("http://34.82.81.113:3000/students");
+    students.value = response.data;
+  } catch (error) {
+    console.error("Error fetching students:", error);
+  }
+});
+</script>
+
+<template>
+ <table class="w-full border-3 text-left bg-slate-300">
+  <thead class="border-b">
+    <tr>
+      <th>Name</th>
+      <th>Group</th>
+    </tr>
+  </thead>
+  <tbody class="bg-slate-100">
+    <tr class="border-2 px-6" v-for="(stud, index) in students" :key="index">
+      <td>{{ stud.name }}</td>
+      <td>{{ stud.group }}</td>
+    </tr>
+  </tbody>
+ </table>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
